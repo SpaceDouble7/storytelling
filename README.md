930f6d3a0ac0024e94e5c0af9827a1307a2be394
SpaceDouble7 <spacexixixi3210>
2015-08-31 17:49:18 +0800

    添加了ip查询接口的说明

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # storytelling
+storytelling中使用了https://www.showapi.com/ 的查询ip的api
 这个小项目主要是辅助批量下载http://www.5ips.net/的评书
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ __author__ = 'Zhao Yu'
 
 import storytelling.storytelling
 
+# 这里使用了https://www.showapi.com/ 的两个api，用于获取外网的ip信息
+# app id和app secret可以从https://www.showapi.com/注册并获得
 st = storytelling.storytelling.Storytelling('app id', 'app secret')
 download_page_url_list = st.get_download_page_list('http://www.5ips.net/ps/36.htm')
 print(download_page_url_list)
